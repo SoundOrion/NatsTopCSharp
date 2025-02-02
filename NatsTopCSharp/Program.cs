318999017fadbef03d9e3663525ece1e9e766561
hatsuyama <hatsuyama>
2025-02-02 15:28:29 +0900

    edit

--- NatsTopCSharp/Program.cs
+++ NatsTopCSharp/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NatsTopCSharp.Services;
 
 namespace NatsTopCSharp;
@@ -9,22 +8,22 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // DIコンテナを構築
-        var serviceCollection = new ServiceCollection();
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                // Optionsはコマンドライン引数からパースしてシングルトンとして登録
+                var options = Options.Parse(args);
+                services.AddSingleton(options);
 
-        // Optionsはコマンドライン引数からパースしてシングルトンとして登録
-        var options = Options.Parse(args);
-        serviceCollection.AddSingleton(options);
-
-        // Engine、DisplayService、App を登録
-        serviceCollection.AddSingleton(new Engine(options.Host, options.Port, options.Conns, options.Delay));
-        serviceCollection.AddSingleton<DisplayService>();
-        serviceCollection.AddSingleton<App>();
-
-        var serviceProvider = serviceCollection.BuildServiceProvider();
+                // Engine、DisplayService、App を登録
+                services.AddSingleton(new Engine(options.Host, options.Port, options.Conns, options.Delay));
+                services.AddSingleton<DisplayService>();
+                services.AddSingleton<App>();
+            })
+            .Build();
 
         // DIで解決したAppクラスを実行
-        var app = serviceProvider.GetRequiredService<App>();
-        await app.RunAsync(displayMode:"");
+        var app = host.Services.GetRequiredService<App>();
+        await app.RunAsync(displayMode: "");
     }
 }
